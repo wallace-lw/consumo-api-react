bf77b085e7dfd7e84f2e405cab9a35d5626457fb
Wallace Leonhard <wallacelw22@gmail.com>
2022-03-23 21:22:29 -0300

    Finalizando o consumo e estilização do projeto. THE END

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,55 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { APIKey } from '../../config/key';
+import { Container } from './styles.js';
 
 function Details() {
   const { id } = useParams();
-  console.log(id);
+  const image_path = "https://image.tmdb.org/t/p/w500";
+  const [movie, setMovie] = useState({});
 
+  useEffect(() => {
+    
+    fetch(`https://api.themoviedb.org/3/movie/ ${id}?api_key=${APIKey}&language=en-US&page=1`)
+      
+      .then(reponse => reponse.json())
+      .then((data) => {
+
+      const { title, overview, poster_path, release_date} = data;
+
+      const movie = {
+        id, 
+        title: title,
+        sinopse: overview,
+        image: `${image_path}${poster_path}`,
+        releaseDate: release_date,
+      }
+
+       setMovie(movie)
+      })
+  }, [id])
+  
   return (
-    <h1>Details</h1>
+    <Container>
+      <div className="movie">
+        <img src={movie.image} alt={movie.sinopse}/>
+        <div className="details">
+          <h1>{movie.title}</h1>
+          <span>Sinopse: {movie.sinopse}</span>
+          <span className="release">Release Date: {movie.releaseDate}</span>
+          <Link to="/">
+            <button>Go Back</button>
+          </Link>
+        </div>
+      </div>
+    </Container>
   );
 }
 
 export default Details;
+
+
+  
+
+
+
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,2 +1,56 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+    
+    margin-top: 10rem;
+    
+    h1 {
+        margin: 3rem 0;
+    }
+
+    .movie {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    img {
+        width: 300px;
+        border-radius: 1rem;
+    }
+
+    .details {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin-left: 4rem;
+        max-width: 50%;
+    }
+
+    button {
+        background-color: #6654da;
+        border: none;
+        cursor: pointer;
+        border-radius: 1rem;
+        color: white;
+        padding: 0.8rem 2rem;
+        margin-top: 1rem;
+        font-size: 100%;
+        transition:all 0.4s;
+    }
+
+    button:hover{
+        transform: scale(1.1);
+        background-color: #5848c2;
+    }
+
+    span {
+        line-height: 130%;
+        margin-bottom: 1rem;
+        font-size: 110%;
+    }
+
+    .release{
+        opacity: 0.5;
+    }
+`
